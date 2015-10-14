be909d5b35b2a8159f11b4369f5ecb2fa4a19db7
wangyan <wyvernnot@gmail.com>
2015-10-14 15:22:36 +0800

    Update message

--- notification.js
+++ notification.js
@@ -2,28 +2,43 @@ var socket = new WebSocket('ws://{host}');
 socket.onmessage = function (msg) {
   var data = JSON.parse(msg.data);
   Notification.requestPermission(function () {
-    var message = data.user_name;
+    var instance;
+    var ACTIONS = {
+      'open': '创建',
+      'close': '关闭',
+      'update': '更新',
+      'reopen': '重新开启'
+    };
+
     switch (data.object_kind) {
       case 'push':
-        message += '提交了代码';
+        instance = new Notification(data.ref.replace('refs/heads/', ''), {
+          body: data.user_name + '推了代码，包含' + data.total_commits_count + '个提交'
+        });
         break;
       case 'tag_push':
-        message += '创建了Tag';
+        instance = new Notification(data.user_name + '创建了标签', {
+          body: data.ref.replace('refs/tags/', '')
+        });
         break;
       case 'issue':
-        message += '创建了问题';
+        instance = new Notification(data.user.name + ACTIONS[data.object_attributes.action] + '了问题:', {
+          body: '#' + data.object_attributes.iid + ' ' + data.object_attributes.title,
+          icon: data.user.avatar_url
+        });
         break;
-      case 'push':
-        message += '提交了合并请求';
+      case 'merge_request':
+        instance = new Notification(data.user.name + ACTIONS[data.object_attributes.action] + '提交了合并请求:', {
+          body: '#' + data.object_attributes.iid + ' ' + data.object_attributes.title,
+          icon: data.user.avatar_url
+        });
         break;
       default:
         break;
     }
-    var instance = new Notification(data.repository.name, {
-      body: message
-    });
+
     setTimeout(function () {
       instance.close();
-    }, 1000);
+    }, 5000);
   });
 }
